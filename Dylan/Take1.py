86320476f8efc04221c76478ad113cda2306c910
dsherry <sharshofski@gmail.com>
2010-04-27 17:02:03 -0400

    Nothing siginificant added, just some notes.

--- Dylan/Take1.py
+++ Dylan/Take1.py
@@ -12,8 +12,17 @@ testVerbSenses = [["run",0],["breathe",1],["meow",2]] ## [[verbSenseName,first_C
 testNounSenses = [["dog",0],["cat",1],["rabbit",2]]
 
 
+## AS OF TUESDAY APRIL 27:
+## I was looking to see what the incoming datatype is, buold around that.
+## Then, finish methods, write tests.
+## unicode strings (run build_lattices.py in ~/goalmining/build_lattices
+
+
 class Lattice:
 
+    ## list of verbSenses, list of nounSenses, and a [(len(verbSenses))x(len(nounSenses))] array
+    ## description of verbSenses/nounSenses:
+    ## [[verbName,first_Sense_Index,second_Sense_Index, third...],...]
     def __init__(self, verbSenses, nounSenses, weights):
         self.verbSenseNum = 0
         self.nounSenseNum = 0
@@ -22,8 +31,6 @@ class Lattice:
         ## each verb gets a list of weights for nouns
         ## default value = None
 
-        ## description of verbSenses/nounSenses:
-        ## [[verbName,first_Sense_Index,second_Sense_Index, third...],...]
         self.verbSenses = verbSenses ## array of strings
         self.nounSenses = nounSenses ## array of strings
 
@@ -33,84 +40,82 @@ class Lattice:
 
 
 	## given a verbSense and nounSense, return the corresponding coordinates in self.weights
-	def getIndices(self, verbSense, nounSense):
-            ## check that verb/noun senses are in the dataset
-            assert (verbSense in self.verbs), "The verb sense %s is not in the dataset." % (verbSense)
-            assert (nounSense in self.nouns), "The noun sense %s is not in the dataset." % (nounSense)
-            return scipy.where(self.verbs==verbSense)[0], scipy.where(self.nouns==nounSense)[0]
+    def getIndices(self, verbSense, nounSense):
+        ## check that verb/noun senses are in the dataset
+        assert (verbSense in self.verbs), "The verb sense %s is not in the dataset." % (verbSense)
+        assert (nounSense in self.nouns), "The noun sense %s is not in the dataset." % (nounSense)
+        return scipy.where(self.verbs==verbSense)[0], scipy.where(self.nouns==nounSense)[0]
 
-        ## gets unique sense number for verb/noun senses
-        def getSenseNumber(self, isVerbSense):
-            if isVerbSense:
-                self.verbSenseNum += 1
-                return str(self.verbSenseNum)
-            self.nounSenseNum += 1
-            return self.nounSenseNum
+    ## gets unique sense number for verb/noun senses
+    def getSenseNumber(self, isVerbSense):
+        if isVerbSense:
+            self.verbSenseNum += 1
+            return str(self.verbSenseNum)
+        self.nounSenseNum += 1
+        return self.nounSenseNum
 
-        ## adds an edge between two senses
-	def add_edge(self, verbName, nounName, weight):
-            ## this function may not be necessary
+    ## adds an edge between two senses
+    def add_edge(self, verbName, nounName, weight):
+        ## this function may not be necessary
 
-            ## check that verb/noun senses are in the dataset
-            assert (verbName in self.verbs), "The verb sense %s is not in the dataset." % (verbName)
-            assert (nounName in self.nouns), "The noun sense %s is not in the dataset." % (nounName)
+        ## check that verb/noun senses are in the dataset
+        assert (verbName in self.verbs), "The verb sense %s is not in the dataset." % (verbName)
+        assert (nounName in self.nouns), "The noun sense %s is not in the dataset." % (nounName)
 
-            ## get the indices of the correct weight (this part is almost correct)
-            indices = scipy.where(self.verbs==verbSense)[0],scipy.where(self.nouns==nounSense)[0]
+        ## get the indices of the correct weight (this part is almost correct)
+        indices = scipy.where(self.verbs==verbSense)[0],scipy.where(self.nouns==nounSense)[0]
 
-            if self.weights[verbIndex][nounIndex] != None: ## already has an edge
-	       	self.weights[verbIndex][nounIndex] = weight
-            ## else, doesn't already have an edge
+        if self.weights[verbIndex][nounIndex] != None: ## already has an edge
             self.weights[verbIndex][nounIndex] = weight
-		
-	## adds a verb or noun class to the existing structure
-	def add_class(self, verb, newWeights = None):
-            ## ***in***
-            ## verb: is the new class a verb (or a noun)?
-            ## newWeights: None is default
-            ## ***actions***
-            ## (should only be added if class is not already present, correct?)
-            ## add either row (verb) or column (noun) to existing structure:
-            ##		-) add to self.verbs/self.nouns list
-            ##		-) add row/column to self.weights matrix, with values from newWeights (or 0s/None)
+        ## else, doesn't already have an edge
+        self.weights[verbIndex][nounIndex] = weight
 
-            ## how should we assign a "name" for the class??
-            ## will be taken care of by verb names -> multiple verb senses implementation
+    ## adds a verb or noun class to the existing structure
+    def add_class(self, verb, newWeights = None):
+        ## ***in***
+        ## verb: is the new class a verb (or a noun)?
+        ## newWeights: None is default
+        ## ***actions***
+        ## (should only be added if class is not already present, correct?)
+        ## add either row (verb) or column (noun) to existing structure:
+        ##		-) add to self.verbs/self.nouns list
+        ##		-) add row/column to self.weights matrix, with values from newWeights (or 0s/None)
 
-            return add_general(verb, newWeights)
+        ## how should we assign a "name" for the class??
+        ## will be taken care of by verb names -> multiple verb senses implementation
 
-        def add_general(verb, newWeights):
-            ## a more general add for both rows and columns
-            a,b = (self.weights.shape[0]+1, self.weights.shape[1]) if verb else (self.weights.shape[0], self.weights.shape[1]+1)
-            self.weights = scipy.resize(self.weights, (a,b))
+        return add_general(verb, newWeights)
 
-            a,b = (self.weights.shape[0], self.weights.shape[1]) if verb else (self.weights.shape[0], self.weights.shape[1])
+    def add_general(verb, newWeights):
+        ## a more general add for both rows and columns
+        a,b = (self.weights.shape[0]+1, self.weights.shape[1]) if verb else (self.weights.shape[0], self.weights.shape[1]+1)
+        self.weights = scipy.resize(self.weights, (a,b))
 
-            if newWeights == None:
-                newWeights = scipy.ones(a)
-            if verb: self.weights[a-1,:] = newWeights
-            else:
-	        self.weights[:,b-1] = newWeights
-            return self.weights
+        a,b = (self.weights.shape[0], self.weights.shape[1]) if verb else (self.weights.shape[0], self.weights.shape[1])
 
-	## removes a verb or noun class from the existing structure
-	def kill_class(self, verbOrNoun, className):
-            ## ***in***
-            ## verbOrNoun: is the new class a verb or a noun?
-            ## className: the "name' of the class
-            ## ***actions***
-            ## (should only be done if class is already present)
-            ## remove associated self.weights row/column
-            ## remove class name from self.verbs/self.nouns
+        if newWeights == None:
+            newWeights = scipy.ones(a)
+        if verb: self.weights[a-1,:] = newWeights
+        else:
+            self.weights[:,b-1] = newWeights
+        return self.weights
 
-            
+    ## removes a verb or noun class from the existing structure
+    def kill_class(self, verbOrNoun, className):
+        ## ***in***
+        ## verbOrNoun: is the new class a verb or a noun?
+        ## className: the "name' of the class
+        ## ***actions***
+        ## (should only be done if class is already present)
+        ## remove associated self.weights row/column
+        ## remove class name from self.verbs/self.nouns
 
-	## merges 2 or more classes into a new class
-	def merge(self, *classes)
-            ## ***in***
-            ## MUST be at least 2 arguments passed into "*classes"
-            ## ***actions***
-            ## add new class, then kill old classes
+    ## merges 2 or more classes into a new class
+    def merge(self, *classes)
+        ## ***in***
+        ## MUST be at least 2 arguments passed into "*classes"
+        ## ***actions***
+        ## add new class, then kill old classes
 
-            ## for now, sum weights
-            ## will deal with multiple senses/pointers issue later
+        ## for now, sum weights
+        ## will deal with multiple senses/pointers issue later
